9927244fd90065fdfe898471834fc8a6dadce795
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2019-09-22 00:24:07 +0200

    dockerfile

--- helloService/config/DB.js
+++ helloService/config/DB.js
@@ -1,6 +1,6 @@
 const HELLO_SERVICE_NAME = 'hello-service';
 const HELLO_SERVICE_DB_NAME = 'hello-mongo';
-const HELLO_SERVICE_DB_PORT = 27017;
+const HELLO_SERVICE_DB_PORT = 27018;
 
 const URL = `mongodb://${HELLO_SERVICE_DB_NAME}:${HELLO_SERVICE_DB_PORT}/`;
 const DB_NAME = HELLO_SERVICE_NAME;
