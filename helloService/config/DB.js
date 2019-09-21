811669330c3b6552cab417199d76b5bda1f85f83
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2019-09-21 23:11:57 +0200

    docker file modifications

--- helloService/config/DB.js
+++ helloService/config/DB.js
@@ -1,7 +1,8 @@
 const HELLO_SERVICE_NAME = 'hello-service';
 const HELLO_SERVICE_DB_NAME = 'hello-mongo';
+const HELLO_SERVICE_DB_PORT = 27017;
 
-const URL = `mongodb://${HELLO_SERVICE_DB_NAME}/`;
+const URL = `mongodb://${HELLO_SERVICE_DB_NAME}:${HELLO_SERVICE_DB_PORT}/`;
 const DB_NAME = HELLO_SERVICE_NAME;
 
 module.exports = {
--- welcomeService/config/DB.js
+++ welcomeService/config/DB.js
@@ -1,7 +1,8 @@
 const WELCOME_SERVICE_NAME = 'welcome-service';
 const WELCOME_SERVICE_DB_NAME = 'welcome-mongo';
+const WELCOME_SERVICE_DB_PORT = 27018;
 
-const URL = `mongodb://${WELCOME_SERVICE_DB_NAME}/`;
+const URL = `mongodb://${WELCOME_SERVICE_DB_NAME}:${WELCOME_SERVICE_DB_PORT}/`;
 const DB_NAME = WELCOME_SERVICE_NAME;
 
 module.exports = {
